3d5af802d97f620b1f3125a4d8415c382c171cbd
Matthew Hunger <matt.hunger@gmail.com>
2017-12-02 13:56:46 +0000

    automatically backed up by learn

--- parrot.rb
+++ parrot.rb
@@ -1,7 +1,7 @@
 # Create method `parrot` that outputs a given phrase and
 # returns the phrase
 
-def parrot(argument="Squawk!")
-  puts "Squawk!"
-  return "Squawk!"
+def parrot(word="Squawk!")
+  puts "#{word}"
+  return word
 end
